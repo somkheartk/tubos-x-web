babca7302b6c2c99ddd400a5bcf90a1b83a1acaa
Somkheart Kraisin <somkheartkra@gosoft.co.th>
2026-04-26 12:21:33 +0700

    feat: add build and SSG manifests for development environment

--- .next/types/link.d.ts
+++ .next/types/link.d.ts
@@ -30,8 +30,8 @@ declare namespace __next_route_internal_types__ {
 
   type StaticRoutes = 
     | `/`
-    | `/admin/pos`
     | `/admin`
+    | `/admin/pos`
     | `/admin/purchase-orders`
     | `/admin/stock`
   type DynamicRoutes<T extends string = string> = never
